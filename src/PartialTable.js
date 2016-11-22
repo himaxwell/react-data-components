e314242cf39368fc09138209cadfd0f9cf8cc046
James Dullaghan <james@fldlvl.com>
2016-11-22 13:25:03 -0700

    Modify layout of partial table

--- src/PartialTable.js
+++ src/PartialTable.js
@@ -7,57 +7,32 @@ export default class PartialTable extends Component {
   render() {
     const {
       onFilter, onPageSizeChange, onPageNumberChange, onSort,
-      pageLengthOptions, columns, keys, buildRowOptions
+      pageLengthOptions, columns, keys, buildRowOptions, tableClassName
     } = this.props;
-
-    let { tableClassName } = this.props;
 
     const {
       page, pageSize, pageNumber,
       totalPages, sortBy, filterValues,
     } = this.props.data;
 
-    if (typeof tableClassName === 'undefined') return 'table';
-
     return (
       <div className="container">
-        <div className="row">
-          <div className="col-xs-4">
-            <div>
-              <label htmlFor="page-menu">Page size:</label>
-              <select
-                id="page-menu"
-                value={pageSize}
-                onChange={onPageSizeChange}
-              >
-                {pageLengthOptions.map(opt =>
-                  <option key={opt} value={opt}>
-                    {opt === 0 ? 'All' : opt}
-                  </option>
-                )}
-              </select>
+        <div className="search-form-group">
+          <label className="sr-only" htmlFor="search">Search</label>
+          <div className="input-group">
+            <input
+              type="search"
+              className="form-control"
+              id="search-field"
+              value={filterValues.globalSearch}
+              onChange={onFilter.bind(null, 'globalSearch')} />
+            <div className="input-group-addon">
+              <span className="glyphicon glyphicon-search" />
             </div>
-            <div>
-              <label htmlFor="search-field">Search:</label>
-              <input
-                id="search-field"
-                type="search"
-                value={filterValues.globalSearch}
-                onChange={onFilter.bind(null, 'globalSearch')}
-              />
-            </div>
-          </div>
-          <div className="col-xs-8">
-            <Pagination
-              className="pagination pull-right"
-              currentPage={pageNumber}
-              totalPages={totalPages}
-              onChangePage={onPageNumberChange}
-            />
           </div>
         </div>
         <Table
-          className={tableClassName}
+          className={`table ${tableClassName}`}
           dataArray={page}
           columns={columns}
           keys={keys}
@@ -70,3 +45,32 @@ export default class PartialTable extends Component {
   }
 
 }
+
+// <div className="row">
+//   <div className="col-xs-4">
+//     <div>
+//       <label htmlFor="page-menu">Page size:</label>
+//       <select
+//         id="page-menu"
+//         value={pageSize}
+//         onChange={onPageSizeChange}
+//       >
+//         {pageLengthOptions.map(opt =>
+//           <option key={opt} value={opt}>
+//             {opt === 0 ? 'All' : opt}
+//           </option>
+//         )}
+//       </select>
+//     </div>
+//     <div>
+//     </div>
+//   </div>
+//   <div className="col-xs-8">
+//     <Pagination
+//       className="pagination pull-right"
+//       currentPage={pageNumber}
+//       totalPages={totalPages}
+//       onChangePage={onPageNumberChange}
+//     />
+//   </div>
+// </div>
